a79fbf3baae960efbe1682669b805d4163d4dfd6
18ADP <162902005+18ADP@users.noreply.github.com>
2024-03-16 10:47:57 +0530

    Add files via upload

---
+++ Computer_Organization_And_Architecture/lab2obj2.asm
@@ -0,0 +1,21 @@
+;Adarsh Pujari
+;2241016496
+
+mov ax,0000h
+mov ds ,ax 
+
+mov si, 2000h
+mov al, [si]
+mov cl, 08h
+mov ch, 0000h
+
+loop1:shr al,01h
+      jc loop2
+      inc ch
+
+loop2:dec cl
+      jnz loop1
+      inc si
+      mov [si],ch
+
+hlt
