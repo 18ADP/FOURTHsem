e15ddf1f3f1d18b57a47ed6cd2d25e4fdae5ad34
18ADP <162902005+18ADP@users.noreply.github.com>
2024-03-18 09:58:38 +0530

    Add files via upload

---
+++ CSW2_ClassLec/Error_Handeling.java
@@ -0,0 +1,21 @@
+package oops;
+
+public class Error_Handeling {
+
+	public static void main(String[] args) {
+		int v1=5+4*3/2;
+		int v2=(5+4)*3/2;
+		int v3=(5+4)*(3/2);
+		int v4=(5+(4*3))/2;
+		System.out.println(v1+" "+v2+" "+v3+" "+v4);
+		
+		
+		
+		for(int c=1;c<=10;c++) {
+			System.out.print(" Counter is: "+ c );
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
