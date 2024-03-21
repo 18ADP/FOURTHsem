cb7a98400c308e7829aace81831133afe3aadfeb
18ADP <162902005+18ADP@users.noreply.github.com>
2024-03-21 18:53:38 +0530

    Update

--- AD2/Lab-4/Rabin_Karp.java
+++ AD2/Lab-4/Rabin_Karp.java
@@ -1,80 +1,64 @@
-// Following program is a Java implementation
-// of Rabin Karp Algorithm given in the CLRS book
+//Rabin-Karp string matching algorithm
 
-public class Rabin_Karp {
-    // d is the number of characters in the input alphabet
-    public final static int d = 256;
+import java.util.*;
 
-    /*
-     * pat -> pattern
-     * txt -> text
-     * q -> A prime number
-     */
-    static void search(String pat, String txt, int q) {
-        int M = pat.length();
-        int N = txt.length();
-        int i, j;
-        int p = 0; // hash value for pattern
-        int t = 0; // hash value for txt
-        int h = 1;
+class RabinKarp
+{
+    public static int search(String text, String pattern)
+    {
+        char[] textArray = text.toCharArray();
+        char[] patternArray = pattern.toCharArray();
 
-        // The value of h would be "pow(d, M-1)%q"
-        for (i = 0; i < M - 1; i++)
-            h = (h * d) % q;
+        int n = text.length(), m = pattern.length(), prime = 101, pown = 1, textHash = 0, patternHash = 0, i, j;
 
-        // Calculate the hash value of pattern and first
-        // window of text
-        for (i = 0; i < M; i++) {
-            p = (d * p + pat.charAt(i)) % q;
-            t = (d * t + txt.charAt(i)) % q;
+        if (m == 0 || m > n)
+            return -1;
+
+        for (i = 0; i < m - 1; i++)
+            pown = (pown << 1) % prime;
+
+        for (i = 0; i < m; i++)
+        {
+            patternHash = ((patternHash << 1) + patternArray[i]) % prime;
+            textHash = ((textHash << 1) + textArray[i]) % prime;
         }
 
-        // Slide the pattern over text one by one
-        for (i = 0; i <= N - M; i++) {
-
-            // Check the hash values of current window of
-            // text and pattern. If the hash values match
-            // then only check for characters one by one
-            if (p == t) {
-                /* Check for characters one by one */
-                for (j = 0; j < M; j++) {
-                    if (txt.charAt(i + j) != pat.charAt(j))
+        for (i = 0; i <= (n - m); i++)
+        {
+            if (textHash == patternHash)
+            {
+                for (j = 0; j < m; j++)
+                    if (textArray[i + j] != patternArray[j])
                         break;
-                }
 
-                // if p == t and pat[0...M-1] = txt[i, i+1,
-                // ...i+M-1]
-                if (j == M)
-                    System.out.println(
-                            "Pattern found at index " + i);
+                if (j == m)
+                    return i;
+
             }
 
-            // Calculate hash value for next window of text:
-            // Remove leading digit, add trailing digit
-            if (i < N - M) {
-                t = (d * (t - txt.charAt(i) * h)
-                        + txt.charAt(i + M))
-                        % q;
-
-                // We might get negative value of t,
-                // converting it to positive
-                if (t < 0)
-                    t = (t + q);
+            if (i < n - m)
+            {
+                textHash = (((textHash - textArray[i] * pown) << 1) + textArray[i + m]) % prime;
+                if (textHash < 0)
+                    textHash += prime;
             }
         }
+
+        return -1;
     }
 
-    /* Driver Code */
-    public static void main(String[] args) {
-        String txt = "GEEKS FOR GEEKS";
-        String pat = "GEEK";
-
-        // A prime number
-        int q = 101;
-
-        // Function Call
-        search(pat, txt, q);
+    public static void main(String[] args)
+    {
+        String river = "Hello there how are you";
+        String drop = "how";
+        int index = search(river, drop);
+        if (index != -1)
+            System.out.println("Pattern found at index: " + (index+1));
+        else
+            System.out.println("Pattern not found.");
     }
 }
 
-// This code is contributed by nuclode
+/*Output:
+Pattern found at index: 13
+*/
